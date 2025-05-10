e2f45d28655cc20ecc0e4be013c4674ab886983e
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-05-10 11:20:27 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Skope-AI",
+  description: "Share AI-powered videos with the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-[#171717] antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
