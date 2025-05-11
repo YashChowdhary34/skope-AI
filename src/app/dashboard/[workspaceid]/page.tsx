1641e139f6f850d55894305ff6bdab13a955a10d
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-05-11 12:05:21 +0530

    added user server action

--- src/app/dashboard/[workspaceid]/page.tsx
+++ src/app/dashboard/[workspaceid]/page.tsx
@@ -1,10 +1,19 @@
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  //authentication
+  const auth = await onAuthenticateUser();
+  if (auth.status === 200 || auth.status === 201) {
+    return redirect(`/dashboard/${auth.user?.firstname}${auth.user?.lastname}`);
+  }
+  if (auth.status === 400 || auth.status === 500 || auth.status === 404) {
+    return redirect("/auth/sign-in");
+  }
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>This is the dashboard page.</p>
     </div>
   );
 }
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  let prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
-export const client = globalThis.prisma || new PrismaClient();
+export const client = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = client;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
